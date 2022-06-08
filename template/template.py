8a118eaabfe69b089cb1a9b2dc4661508686746c
JakeJing <350956009@qq.com>
2022-06-08 10:47:03 +0200

    update template.py

--- template/template.py
+++ template/template.py
@@ -1,28 +1,28 @@
 ---
-title: "Template python script"
+title: "Template pmd file for python"
 author: "Yingqi Jing"
-date:
+date: 21.02.2021
 header-includes:
-  - \usepackage{textcomp}
-  - \renewcommand{\thefigure}{S\arabic{figure}}
-  - \renewcommand{\thetable}{S\arabic{table}}
-  - \renewcommand{\thesection}{S\arabic{section}}
-  - \renewcommand{\thesubsection}{S\arabic{section}.\arabic{subsection}}
-  - \usepackage{tocloft}
-  - \settowidth{\cftsecnumwidth}{S10x}
+- \usepackage{textcomp}
+- \renewcommand{\thefigure}{S\arabic{figure}}
+- \renewcommand{\thetable}{S\arabic{table}}
+- \renewcommand{\thesection}{S\arabic{section}}
+- \renewcommand{\thesubsection}{S\arabic{section}.\arabic{subsection}}
+- \usepackage{tocloft}
+- \settowidth{\cftsecnumwidth}{S10x}
 output:
-  pdf_document:
-   fig_crop: true
-   fig_caption: true
-   latex_engine: xelatex
-   toc: true
-   toc_depth: 4
-   number_section: true
-   pandoc_args: ["--variable=lof","--variable=lot","--bibliography=/Users/jakejing/switchdrive/bib/references.bib", "--csl=/Users/jakejing/switchdrive/bib/unified-style-linguistics.csl"]
+pdf_document:
+fig_crop: true
+fig_caption: true
+latex_engine: xelatex
+toc: true
+toc_depth: 4
+number_section: true
+pandoc_args: ["--variable=lof","--variable=lot","--bibliography=/Users/jakejing/switchdrive/bib/references.bib", "--csl=/Users/jakejing/switchdrive/bib/unified-style-linguistics.csl"]
 ---
 \clearpage
 
-# %% name = "setup", echo = False, include=False
+# %% echo = False, include=False
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import matplotlib.pyplot as plt
@@ -30,33 +30,22 @@ from matplotlib import cm
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
 # %%
 
-# %% name = "newchunk"
-print("hello world")
-# %%
-
 Introduction
-============
+===========
 
 This an example of a script that can be published using `Pweave`(http://mpastell.com/pweave). The script can be executed.
 
 Data and Methods
-=============
+============
 
-# %% caption = "An example 3D plot", f_size = (6.5, 4.5)
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-X = np.arange(-5, 5, 0.25)
-Y = np.arange(-5, 5, 0.25)
-X, Y = np.meshgrid(X, Y)
-Z = np.sin(np.sqrt(X**2 + Y**2))
-surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
-          linewidth=0, antialiased=False)
-fig.colorbar(surf, shrink=0.5, aspect=5)
-plt.show()
+
+# %% name = "for loop"
+for i in range(10):
+    print(i)
 # %%
 
 Results
-============
+=========
 
 Discussions
-============
+==========
